d46bfc52322fcf512df70af567d0a68e7c3d0ade
SofiBernardoni <s296685@studenti.polito.it>
2024-12-22 18:05:26 +0100

    fine constraints

--- project_basic_temp/prj_temp/data/uploading_data.py
+++ project_basic_temp/prj_temp/data/uploading_data.py
@@ -1,2 +1,3 @@
 #caricare camere non compatibili come set con id numerici
 #trasformare direttamente l'age group in numero: age_num=self.h.age_groups[self.h.occupants[id_occ]["age_group"]] #set initial room ages
+#mettere in working shift come id il numero e non la stringa come codice infermiere
--- project_basic_temp/prj_temp/instances/hospital.py
+++ project_basic_temp/prj_temp/instances/hospital.py
@@ -44,11 +44,22 @@ class Hospital():
     n_patients=0
     patients=[{}]*n_patients #list of dictionaries of patients position=id
 
+    #weights
+    weights={
+        "room_mixed_age": 0,
+        "room_nurse_skill": 0,
+        "continuity_of_care": 0,
+        "nurse_eccessive_workload": 0,
+        "open_operating_theater": 0,
+        "surgeon_transfer": 0,
+        "patient_delay": 0,
+        "unscheduled_optional": 0
+    }
     ##############
 
 
     def __init__(self, n_days,skill_levels, shift_types, age_groups, n_nurses, nurses_skill_levels, working_shifts, n_surgeons, surgeons_availability,
-                 n_op_theaters, op_theaters_availability, n_rooms,rooms_capacity,n_occupants,occupants,n_patients,patients):
+                 n_op_theaters, op_theaters_availability, n_rooms,rooms_capacity,n_occupants,occupants,n_patients,patients, weights):
         self.n_days = n_days
         self.skill_levels =skill_levels
         self.shift_types = shift_types
@@ -68,6 +79,7 @@ class Hospital():
         self.occupants = occupants
         self.n_patients = n_patients
         self.patients = patients
+        self.weights=weights
 
 
 '''
@@ -98,6 +110,7 @@ class Scheduling():
 
     room_to_patient=[[set(), set()]]*h.n_rooms # id=n_room, set1= patients in the room, set2=occupants in the room
 
+    room_occupants=[]*(h.n_occupants) #occupants' room
 
     # constraints PSA
     room_gender=[None]*h.n_rooms
@@ -117,13 +130,19 @@ class Scheduling():
     op_to_surgeon=[set()]*h.n_surgeons #pos= id_surgeon, el: set of op_theater_used_in a day
 
     # global constraints
-    delays=[0]*h.n_patients #pos=id_patients, el:delay
-    unscheduled_patients=set() #set of unscheduled patients
+    delays=[0]*h.n_patients #pos=id_patients, el:delay #S7
+    unscheduled_patients=set() #set of unscheduled patients #S8
 
     #total surgeons_transfer
     tot_tranfer=0 #S6
     tot_op_theater_opened=0 #S5
     tot_diff_age=0 #S1
+    tot_skill_vio=0 #S2
+    tot_eccessive_work=0 #S4
+    tot_continuity_of_care=0 #S3
+
+
+
 
 
     def __init__(self, hospital,pr,ad,op,CN):
@@ -161,6 +180,7 @@ class Scheduling():
             r_id= self.h.occupants[id_occ]["room_id"] #room id of occupant i es: "r00"
             num_r=int(r_id[1:]) #remove the "r" and to_int
             self.room_to_patient[num_r][1].add(id_occ) #occupants in rooms
+            self.room_occupants[id_occ]=num_r # save in occupants' room for exiting occupants
             if self.room_gender[num_r] is None: #set initial gender in the room
                 self.room_gender[num_r]=self.h.occupants[id_occ]["gender"]
             age_num=self.h.occupants[id_occ]["age_group"] #set initial room ages
@@ -258,18 +278,19 @@ class Scheduling():
         d=0
         while self.feasible and d < self.h.n_days:
 
+            #PEOPLE ENTERING
             for np in self.new_patients[d]:
-                room=self.pr[p]
+                room=self.pr[np]
 
-                if self.room_gender[room] is not None and self.room_gender[room] != self.h.patients[p]["gender"]:
+                if self.room_gender[room] is not None and self.room_gender[room] != self.h.patients[np]["gender"]:
                     self.feasible=False #H1
                 elif self.room_gender[room] is None:
-                    self.room_gender[room] = self.h.patients[p]["gender"] #updating data about gender in the room
+                    self.room_gender[room] = self.h.patients[np]["gender"] #updating data about gender in the room
 
                 self.room_to_patient[room][0].add(np) #adding new patient in the room
 
                 #updating data about age in the room
-                age_num = self.h.occupants[p]["age_group"]  # set initial room ages
+                age_num = self.h.occupants[np]["age_group"]  # set initial room ages
                 self.room_age[room].add(age_num)
                 self.room_age_counter[room][age_num] += 1
 
@@ -283,23 +304,100 @@ class Scheduling():
                 r+=1
 
 
+            work_nurses=[0]*self.h.n_nurses #total nurses workload in the day (every nurse does just one shift per day)
+
+            #vediamo poi se si puo unnire
+
+            for r in range(0,self.h.n_rooms):
+                tot_work_room=[0]*self.h.n_shifts # tot workload in a room in every shift for a day
+                max_skill_room=[0]*self.h.n_shifts  #max skill level required in a room for every shift in a day
+                nurse_seen_day=[0]*self.h.n_shifts #nurses for the room for the day
+
+                for shi in range(0, self.h.n_shifts):
+                    s=(self.h.n_shifts * d) + shi
+                    id_nurse=self.CN[r, s]
+                    if self.h.working_shifts(s).get(id_nurse) is None:    #check if the nurse is working in the shift
+                        self.feasible=False
+
+                    else:
+                        nurse_seen_day[shi]=id_nurse
+
+                if not self.feasible: #exiting because nurses not available
+                    break
+
+                for pa in self.room_to_patient[r][0]: #patients in the room
+                    first_index=self.h.n_shifts*(d-self.ad[pa])
+                    for sh in range(0, self.h.n_shifts):
+                        tot_work_room[sh] += self.h.patients[pa]["workload_produced"][first_index+sh] # adding workload requested in the shift
+                        max_skill_room[sh] =max(max_skill_room[sh], self.h.patients[pa]["skill_level_required"][first_index+sh]) #updating skill level requested in the shift
+                        self.nurse_to_patient[pa].add(nurse_seen_day[sh]) #adding the nurse seen by the patient
+
+                for oc in self.room_to_patient[r][1]: #occupants in the room
+                    first_index=self.h.n_shifts*d #admission date=0 for occupants
+                    for sh in range(0, self.h.n_shifts):
+                        tot_work_room[sh] += self.h.occupants[oc]["workload_produced"][first_index+sh] # adding workload requested in the shift
+                        max_skill_room[sh] =max(max_skill_room[sh], self.h.occupants[oc]["skill_level_required"][first_index+sh]) #updating skill level requested in the shift
+                        self.nurse_to_occupant[oc].add(nurse_seen_day[sh]) #adding the nurse seen by the patient
+
+                for t in range(0,self.h.n_shifts):
+                    self.tot_skill_vio += max(max_skill_room[t]-self.h.nurses_skill_levels[nurse_seen_day[t]],0) #S2 difference between skill levels of the nurse and required
+                    work_nurses[nurse_seen_day[t]]+= tot_work_room[t] #adding workload to the nurse
+
+            for k in range(0,self.h.n_shifts):
+                total_shift=self.h.n_shifts*d+k
+                for n in range(0,self.h.n_nurses):
+                    self.tot_eccessive_work += max(0, work_nurses[n]-self.h.working_shifts[total_shift][n]) #S4
 
 
 
 
-            for np in self.exit_patients[d]:
-                self.room_to_patient[self.pr[p]][0].remove(np)  # adding new patient in the room
+            #PEOPLE EXITING
+            for ep in self.exit_patients[d]:
+                room=self.pr[ep]
+                self.room_to_patient[room][0].remove(ep)  # removing patients
+                if len(self.room_to_patient[room][0])+len(self.room_to_patient[room][1]) ==0:
+                    self.room_gender[room] = None
 
-                #da aggiungere eta e genere
-                #
-            # EXIT OCCUPANTS
+                age=self.h.patients[ep]["age_group"]
+                self.room_age_counter[room][age]-=1
+                if self.room_age_counter[room][age]==0:
+                    self.room_age[room].remove(age)
+
+            for eo in self.exit_occupants[d]:
+                room=self.room_occupants[eo]
+                self.room_to_patient[room].remove(eo) #removing occupant
+                if len(self.room_to_patient[room][0]) + len(self.room_to_patient[room][1]) == 0:
+                    self.room_gender[room] = None
+
+                age = self.h.occupants[eo]["age_group"]
+                self.room_age_counter[room][age] -= 1
+                if self.room_age_counter[room][age] == 0:
+                    self.room_age[room].remove(age)
+
 
             d+=1
+        for pp in range(0, self.h.n_patients):
+            self.tot_continuity_of_care += len(self.nurse_to_patient[pp])
+
+        for oo in range(0, self.h.n_occupants):
+            self.tot_continuity_of_care += len(self.nurse_to_occupant[oo])
 
 
+    def fitness(self):
+        tot_delays =sum(self.delays)
+        tot_unscheduled=len(self.unscheduled_patients)
+
+        S6=self.tot_tranfer*self.h.weights["surgeon_transfer"]
+        S5=self.tot_op_theater_opened*self.h.weights["open_operating_theater"]
+        S1=self.tot_diff_age*self.h.weights["room_mixed_age"]
+        S2=self.tot_skill_vio*self.h.weights["room_nurse_skill"]
+        S4=self.tot_eccessive_work*self.h.weights["nurse_eccessive_workload"]
+        S3=self.tot_continuity_of_care*self.h.weights["continuity_of_care"]
+        S7=tot_delays*self.h.weights["patient_delay"]
+        S8=tot_unscheduled*self.h.weights["unscheduled_optional"]
 
 
-
+        return S1+S2+S3+S4+S5+S6+S7+S8
 
 
 
